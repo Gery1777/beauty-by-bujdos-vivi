a0321a71c2796b57b477d75c024da7f7c1fea3b8
Gery1777 <kovacs.gery@hotmail.hu>
2024-07-30 18:08:35 +0200

    adding font-awesome

--- src/components/ResponsiveCarousel.tsx
+++ src/components/ResponsiveCarousel.tsx
@@ -34,9 +34,11 @@
 // }
 
 // export default ResponsiveCarousel;
-import React from "react";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import React, { useState, useEffect, useRef } from "react";
+import "react-responsive-carousel/lib/styles/carousel.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendar } from "@fortawesome/free-regular-svg-icons";
 const images = [
   "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
   "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
@@ -52,7 +54,30 @@ export default function ResponsiveCarousel() {
   return (
     <>
       <div style={{ display: "Flex" }}>
-        <div className="col-3"></div>
+        <div className="col-3">
+          <div
+            style={{
+              display: "Flex",
+              flexDirection: "column",
+              marginRight: "auto",
+              justifyContent: "space-between",
+              height: "16rem",
+            }}
+          >
+            <div>
+              <FontAwesomeIcon
+                icon={faCalendar}
+                style={{ color: "black", width: "40px", height: "40px" }}
+              />
+            </div>
+            <div>
+              <img src={images[1]} style={{ width: "40px", height: "40px" }} />
+            </div>
+            <div>
+              <img src={images[2]} style={{ width: "40px", height: "40px" }} />
+            </div>
+          </div>
+        </div>
         <div className="col-6">
           <Carousel
             showStatus={false}
